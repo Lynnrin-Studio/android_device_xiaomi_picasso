fd95461cc12b1651acb2c9b2c8aa64ce5e32676a
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-17 20:03:01 +0530

    picasso: props: disable settings provider model override

--- product_prop.mk
+++ product_prop.mk
@@ -45,4 +45,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.disableADBLogs=1 \
-    persist.vendor.ims.disableIMSLogs=1
+    persist.vendor.ims.disableIMSLogs=1 \
+    persist.sys.fflag.override.settings_provider_model=false
